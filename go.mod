a8590f1e7011a1548dc87b090a2866971a0b3100
Heeyong <sunrise2575@gmail.com>
2022-06-15 17:28:59 +0000

    complete to distributed zmq

--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.17
 
 require (
 	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/pebbe/zmq4 v1.2.9 // indirect
 	github.com/tidwall/gjson v1.14.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/sjson v1.2.4 // indirect
 	github.com/zeromq/goczmq v4.1.0+incompatible // indirect
 )
--- zeromq_test/master.go
+++ zeromq_test/master.go
@@ -1,61 +1,124 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"sync"
-	"time"
 
 	zmq "github.com/pebbe/zmq4"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
 )
 
-const (
-	STOP_MESSAGE = "!stop"
+func Map2JSON(input map[string]string) string {
+	output := ""
+	for k, v := range input {
+		output, _ = sjson.Set(output, k, v)
+	}
+	return output
+}
+
+// only flat json works
+func JSON2Map(input string) map[string]string {
+	output := map[string]string{}
+	for k, v := range gjson.Parse(input).Map() {
+		output[k] = v.String()
+	}
+	return output
+}
+
+var (
+	MY_HOSTNAME, MY_PID = "", ""
 )
+
+func RecvSend(sock *zmq.Socket, response func(map[string]string) map[string]string) {
+}
 
 func main() {
-	zmq_ctx, _ := zmq.NewContext()
+	MY_HOSTNAME, _ = os.Hostname()
+	MY_PID = strconv.Itoa(os.Getpid())
 
-	// scatter job
-	sock_push, _ := zmq_ctx.NewSocket(zmq.PUSH)
-	sock_push.Bind("tcp://*:5000")
+	SERVER_PORT, DIRECTORY := "", ""
+	flag.StringVar(&SERVER_PORT, "port", "5000", "master port")
+	flag.StringVar(&DIRECTORY, "dir", "/fifi/.media/", "File root directory")
 
-	// gather job
-	sock_pull, _ := zmq_ctx.NewSocket(zmq.PULL)
-	sock_pull.Bind("tcp://*:6000")
+	ctx, _ := zmq.NewContext()
+	sock, _ := ctx.NewSocket(zmq.REP)
+	sock.Bind("tcp://*:" + SERVER_PORT)
 
+	chan_fp := make(chan string, 16)
+
+	// search files recursively
 	var wg sync.WaitGroup
-
-	dp_in := "/fifi/.media"
-
-	// job scatter
 	wg.Add(1)
 	go func() {
-		defer wg.Done()
-		filepath.Walk(dp_in, func(fp_in string, f_info os.FileInfo, err error) error {
+		defer func() {
+			wg.Done()
+		}()
+
+		files := 0
+
+		filepath.Walk(DIRECTORY, func(fp_in string, f_info os.FileInfo, err error) error {
 			if f_info.IsDir() {
 				return nil
 			}
-			sock_push.Send(fp_in, 0)
-			log.Println("[VENT] send message:", fp_in)
-			time.Sleep(time.Millisecond * 10) // for fair balancing
+			if files < 10 {
+				chan_fp <- fp_in
+				files++
+			}
 			return nil
 		})
-		sock_push.Send("stop", 0)
-		sock_push.Close()
 	}()
 
-	// job gather
 	wg.Add(1)
 	go func() {
-		defer wg.Done()
+		defer func() {
+			wg.Done()
+		}()
 		for {
-			msg, _ := sock_pull.Recv(0)
-			if msg == "stop" {
-				break
+			// Must Recv
+			recv_json, _ := sock.Recv(0)
+			recv := JSON2Map(recv_json)
+
+			// Prepare response
+			send_payload := map[string]string{}
+			send_payload["hostname"] = MY_HOSTNAME
+			send_payload["pid"] = MY_PID
+
+			switch recv["req"] {
+			case "job_want":
+				select {
+				case fp := <-chan_fp:
+					send_payload["res"] = "true"
+					send_payload["file_path"] = fp
+					log.Println("GIVE_JOB", fp)
+				default:
+					send_payload["res"] = "false"
+					log.Println("NO_JOB")
+				}
+
+			case "job_done":
+				log.Println("COMPLETE", recv["file_path"])
+
+			case "killed":
+				// retry file
+				log.Println("GOT RETRY FILE")
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					chan_fp <- recv["file_path_incomplete"]
+				}()
+
+			default:
+				// ignore
+				//send_payload["res"] = "wrong_req"
 			}
-			log.Println("[SINK] received:", msg)
+
+			// Must Send
+			sock.Send(Map2JSON(send_payload), 0)
 		}
 	}()
 
